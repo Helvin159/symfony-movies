286aa98db464127c8e998f1ba7ff8ed882648058
helvin159 <helvin159@gmail.com>
2024-04-15 14:10:07 -0400

    updates, need to fix postcss errors

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -69,6 +69,65 @@ class MoviesController extends AbstractController
     }
 
 
+    #[Route('/movies/edit/{id}',  name: 'edit_movie')]
+    public function edit($id, Request $request):Response 
+    {
+        $movie = $this->movieRepository->find($id);
+        $form = $this->createForm(MovieFormType::class, $movie);
+
+        $form->handleRequest($request);
+        $imagePath = $form->get('imagePath')->getData();
+
+        $file = $this->getParameter('kernel.project_dir') . $movie->getImagePath();
+
+        if($form->isSubmitted() && $form->isValid()){
+            if($imagePath){
+               if($movie->getImagePath() !== null){
+                    if(file_exists($file) !== null) {
+                            $this->GetParameter('kernel.project_dir') . $movie->getImagePath();
+
+                            $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+
+                            try{
+                                $imagePath->move(
+                                    $this->getParameter('kernel.project_dir') . '/public/uploads',
+                                    $newFileName
+                                    );
+                            } catch(FileException $e){
+                                    return new Response($e->getMessage());
+                            }
+
+                            $movie->setImagePath('/uploads/' . $newFileName);
+                            $this->em->flush();
+
+                            return $this->redirectToRoute('movies');
+                    }
+               }
+            }else{
+                $movie->setTitle($form->get('title')->getData());
+                $movie->setRealeaseYear($form->get('realeaseYear')->getData());
+                $movie->setDescription($form->get('description')->getData());
+
+                $this->em->flush();
+                return $this->redirectToRoute('movies');
+            }
+        }
+        return $this->render('movies/edit.html.twig', [
+            'movie'=>$movie,
+            'form'=>$form->createView()
+        ]);
+    }
+
+    #[Route('/movies/delete/{id}', methods:['GET', 'DELETE'],  name: 'delete_movie')]
+    public function delete($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        $this->em->remove($movie);
+        $this->em->flush();
+
+        return $this->redirectToRoute('movies');
+    }
+
     #[Route('/movies/{id}', methods:['GET'], name: 'movie')]
     public function show($id):Response
     {
